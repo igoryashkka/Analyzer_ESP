9970d7aae920a07645d91bc5740ff7cf1c18c5a1
LilYa777 <LilYa75310@fmail.com>
2022-09-24 23:52:00 +0300

    add_define

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,10 @@
 #include <WiFi.h>
 #include <Adafruit_ADS1X15.h>
 #include "main.h"
-
+#include "deeefine.h"
 
 //____________________________________________________
-#define pin_voltageOntransistor 1 //напруга на транзисторі
-#define pin_voltageOn_CO1 2 //напруга на першому датчику
-#define pin_voltageOn_CO2 3 //напруга на другому датчику
-#define period1 60*1000L
-#define period2 90*1000L
-#define MQ7_LOAD_RESISTOR 10000
-#define MQ7_CLEANAIR_RSRO 11.7
-#define MQ7_REFERENCE_VOLTAGE 3.3
-#define MQ7_LOAD_RESISTOR 10000
-#define PWM_SIG 15
-#define period1 60*1000L
-#define period2 90*1000L
-#define period3 1000L
-#define period4 3000L
+
 //aboba
 //aboba
 Adafruit_ADS1015 ads;
