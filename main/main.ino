52452fdfa7acd93564c1f3a04584f2be3cea5b6a
igoryash <igor404ik@gmail.com>
2022-09-24 18:00:05 +0300

    test branch dev_markup

--- main/main.ino
+++ main/main.ino
@@ -70,6 +70,8 @@ xTaskCreatePinnedToCore(Task2code,"main_logic",10000,NULL,1,&Task2,1);
   
  
 }
+//  test markup
+
 void Task1code( void * pvParameters ){
  // Serial.print("Task1 running on core ");
  // Serial.println(xPortGetCoreID());
@@ -109,6 +111,7 @@ void Task1code( void * pvParameters ){
             client.println("</h5> <div class=\"fakeimg\" style=\"height:800px;\">");
             client.println("<div> <canvas id=\"myChart\"></canvas> </div> <script> const labels = [ '00:01','00:02','00:03','00:04','00:05','00:06','00:07','00:08','00:09','00:10','00:11','00:12','00:13','00:14','00:15','00:16','00:17','00:18','00:19','00:20','00:21','00:22','00:23','00:24','00:25','00:26','00:27','00:28','00:29','00:30','00:31','00:32','00:33','00:34','00:35','00:36','00:37','00:38','00:39','00:40','00:41','00:42','00:43','00:44','00:45','00:46','00:47','00:48','00:49','00:50','00:51','00:52','00:53','00:54','00:55','00:56','00:57','00:58','00:59','01:00','01:01','01:02','01:03','01:04','01:05','01:06','01:07','01:08','01:09','01:10','01:11','01:12','01:13','01:14','01:15','01:16','01:17','01:18','01:19','01:20','01:21','01:22','01:23','01:24','01:25','01:26','01:27','01:28','01:29','01:30', ]; const data = { labels: labels, datasets: [{ label: 'My First dataset', backgroundColor: 'rgb(255, 99, 132)', borderColor: 'rgb(255, 99, 132)',");
             client.println("  data: [");
+            //  TODO : create a loop 
             client.println(arr[0]); client.println(",");client.println(arr[1]); client.println(",");client.println(arr[2]); client.println(",");client.println(arr[3]); client.println(",");client.println(arr[4]); client.println(",");client.println(arr[5]); client.println(",");client.println(arr[6]); client.println(",");client.println(arr[7]); client.println(",");client.println(arr[8]); client.println(",");client.println(arr[9]); client.println(",");
             client.println(arr[10]); client.println(",");client.println(arr[11]); client.println(",");client.println(arr[12]); client.println(",");client.println(arr[13]); client.println(",");client.println(arr[14]); client.println(",");client.println(arr[15]); client.println(",");client.println(arr[16]); client.println(",");client.println(arr[17]); client.println(",");client.println(arr[18]); client.println(",");client.println(arr[19]); client.println(",");
             client.println(arr[20]); client.println(",");client.println(arr[21]); client.println(",");client.println(arr[22]); client.println(",");client.println(arr[23]); client.println(",");client.println(arr[24]); client.println(",");client.println(arr[25]); client.println(",");client.println(arr[26]); client.println(",");client.println(arr[27]); client.println(",");client.println(arr[28]); client.println(",");client.println(arr[29]); client.println(",");
